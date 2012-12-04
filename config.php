58a4a50f0994b2867432a73f177c694efb595c03
Raphael <macaraphael@hotmail.com>
2012-12-04 10:00:10 -0200

    Update config.php
    Alterações

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 class SConfig{
     public $systemname = 'Gerenciamento';
-    public $host_email = 'smtp.gmail.com';
-    public $port_access = '25';
-    public $user_email = 'raphaelmaracaipe@gmail.com';
-    public $pass_email = '123456';
+    public $host_email = '';
+    public $port_access = '';
+    public $user_email = '';
+    public $pass_email = '';
     public $typedb  = 'mysql';
     public $host = 'localhost';
     public $user = 'root';
